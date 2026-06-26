05e51e68942f75731df873fc14abab36c57a553f
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-06-26 17:33:19 +0530

    Add initial main file

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    // TODO: 1. Add Student Type
+
+    // TODO: 2. Student Registration List (Linked List)
+
+    // TODO: 3. Waiting Queue (Linked List-Based Queue)
+
+    // TODO: 4. Drop History Stack (Linked List-Based Stack)
+
+    // TODO: 5. Main Menu
+
+    return 0;
+}
